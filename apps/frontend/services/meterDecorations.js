ef08db55e0e1c9e95514ca16f93f402f15cea980
illuminatianon <me@illuminati.com>
2025-07-06 16:05:54 -0400

    Delete old apps

--- apps/frontend/services/meterDecorations.js
+++
@@ -1,7 +0,0 @@
-// This file is a placeholder for future implementation
-// We're using a simpler approach for now to avoid compatibility issues
-
-export function createMeterDecorations (meterStore) {
-  // Return a simple extension that doesn't use complex CodeMirror features
-  return [];
-}
